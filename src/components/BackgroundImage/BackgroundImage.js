3d3e08ec4eb75d5ffcbf22b8029cc22397b2706a
wyang0917 <yangwang0917@gmail.com>
2024-06-02 14:55:42 +1000

    update background.js className

--- src/components/BackgroundImage/BackgroundImage.js
+++ src/components/BackgroundImage/BackgroundImage.js
@@ -3,8 +3,8 @@ import bg from '../../assets/background.png'
 
 const BackgroundImage = ({children}) => {
   return (
-    <div className="min-h-screen bg-cover flex items-center justify-center font-alimama w-full opacity-75 bg-gradient-to-r from-indigo-300 to-indigo-600">
-      <img src={bg}  alt='cloud' className='fixed bottom-0 right-0 -z-10'></img>
+    <div className="min-h-screen bg-cover flex items-center justify-center font-alimama w-full opacity-75 bg-gradient-to-r from-indigo-300 to-indigo-600 relative">
+      <img src={bg}  alt='cloud' className='fixed bottom-0 right-0 -z-10 object-contain '></img>
       {children}
     </div>
   )
--- src/components/WeatherCard/WeatherCard.js
+++ src/components/WeatherCard/WeatherCard.js
@@ -7,7 +7,7 @@ import OtherCities from './OtherCities/OtherCities';
 const WeatherCard = () => {
   console.log('weather card rendered');
   return (
-    <div className='bg-slate-200 w-2/3 max-w-5xl h-auto p-8 rounded-3xl flex'>
+    <div className='bg-slate-200 w-2/3 max-w-5xl h-auto p-8 rounded-3xl flex '>
       <CurrentCity />
       <div className=' w-2/3 px-6'>
       <Forecast />
