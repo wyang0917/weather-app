92ea1c32f517d9b315e286762472105f87e6cf27
wyang0917 <yangwang0917@gmail.com>
2024-06-04 01:19:58 +1000

    update SearchBar css and add media responsive

--- src/components/WeatherCard/components/SearchBar/SearchBar.js
+++ src/components/WeatherCard/components/SearchBar/SearchBar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SearchBar = () => {
   return (
-    <form className='mt-28 w-1/2 relative'>
+    <form className='w-1/2 relative max-md:w-full'>
       <input className='outline-none rounded-lg py-1 px-4 w-full' placeholder='search for a city'></input>
       <button className='py-0.5 px-3 text-white bg-violet-900 rounded-lg  absolute inset-y-px right-px '>search</button>
     </form>
