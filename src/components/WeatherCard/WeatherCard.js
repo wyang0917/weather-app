9b0867d410a84c96a6fabd88869fc2692d7b2e35
wyang0917 <yangwang0917@gmail.com>
2024-06-04 15:17:51 +1000

    connect mocks data in CurrentCity,replace the hardcoding,add some meta icon in assets

--- src/components/WeatherCard/WeatherCard.js
+++ src/components/WeatherCard/WeatherCard.js
@@ -1,21 +1,24 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import CurrentCity from './components/CurrentCity';
 import Forecast from './components/Forecast';
 import SearchBar from './components/SearchBar';
 import OtherCities from './components/OtherCities';
-import getCurrent from '../../apis/getCurrent';
+import currentMockData from '../../mocks/current.json';
+import forecastMockData from '../../mocks/forecast.json';
 
 const WeatherCard = () => {
-  // useEffect(()=>{
-  //   getCurrent()
-  //   .then((data)=>{
-  //     console.log(data);
-  //   })
-  // },[])
-
   return (
     <div className="bg-slate-200  max-w-5xl h-auto px-6 py-5 rounded-3xl flex max-md:flex-col max-md:w-80 text-black">
-      <CurrentCity />
+      <CurrentCity
+        temp={currentMockData.current.temp_c}
+        minTemp={forecastMockData.forecast.forecastday[0].day.mintemp_c}
+        maxTemp={forecastMockData.forecast.forecastday[0].day.maxtemp_c}
+        icon={currentMockData.current.condition.icon}
+        humidityDesc={currentMockData.current.humidity}
+        windDesc={currentMockData.current.wind_kph}
+        PMIconDesc={currentMockData.current.uv}
+        somatosensoryIconDesc={currentMockData.current.feelslike_c}
+      />
       <div className=" w-2/3 pl-6 max-md:w-full  max-md:pl-0 flex flex-col justify-between">
         <Forecast />
         <SearchBar />
--- src/components/WeatherCard/components/CurrentCity/components/Meta/Meta.js
+++ src/components/WeatherCard/components/CurrentCity/components/Meta/Meta.js
@@ -4,19 +4,19 @@ import PMIcon from '../../../../../../assets/meta_icon/PM2.5.svg'
 import somatosensoryIcon from '../../../../../../assets/meta_icon/Somatosensory_temperature.svg'
 import windIcon from '../../../../../../assets/meta_icon/wind_speed.svg'
 
-const Meta = ({className}) => {
+const Meta = ({className,humidityDesc,windDesc,PMIconDesc,somatosensoryIconDesc}) => {
   const infos = [
-    {icon:humidityIcon,desc:"85%"},
-    {icon:windIcon,desc:"9km/h"},
-    {icon:PMIcon,desc:"75µg"},
-    {icon:somatosensoryIcon,desc:"26°"},
+    {icon:humidityIcon,desc:`${humidityDesc}%`,alt:'humidityIcon'},
+    {icon:windIcon,desc:`${Math.round(windDesc)}km/h`,alt:'windIcon'},
+    {icon:PMIcon,desc:PMIconDesc,alt:'PMIcon'},
+    {icon:somatosensoryIcon,desc:`${somatosensoryIconDesc}°`,alt:'somatosensoryIcon'},
   ]
   return (
     <div className={className}>
       <ul className='flex justify-around'>
         {infos.map((info,index)=>(
           <li key={index} className='text-center'>
-            <img src={info.icon} alt={`icon${index+1}`} className="mx-auto" />
+            <img src={info.icon} alt={info.alt} className="mx-auto" />
             <span>{info.desc}</span>
           </li>
         ))}
