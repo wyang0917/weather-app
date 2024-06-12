154821829af7df7e78b69123efeb5ebdd1869f5b
wyang0917 <yangwang0917@gmail.com>
2024-06-12 23:06:14 +1000

    use https in stead of  http in API

--- src/apis/getCurrent.js
+++ src/apis/getCurrent.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const apiKey = process.env.REACT_APP_API_KEY;
 
 const getCurrent = async (cityName) => {
-  const url = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${cityName}&days=1`;
+  const url = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${cityName}&days=1`;
   try {
     const response = await axios.get(url);
     return response.data;
--- src/apis/getForecast.js
+++ src/apis/getForecast.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const apiKey = process.env.REACT_APP_API_KEY;
 
 const getForecast = async (cityName) => {
-  const url = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${cityName}&days=5&aqi=yes`;
+  const url = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${cityName}&days=5&aqi=yes`;
   try {
     const response = await axios.get(url);
     return response.data;
